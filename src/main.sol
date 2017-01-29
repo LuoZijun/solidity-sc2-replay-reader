48abe4d2b6f4ebd2345da4ad86cf3e4ccc291e93
Luo <gnulinux@126.com>
2017-01-29 12:01:50 +0800

    [Fix]

--- src/main.sol
+++ src/main.sol
@@ -1,3 +1,4 @@
+pragma solidity ^0.4.0;
 /**
 
 // JavaScript Number To Bits
@@ -15,12 +16,50 @@ function number_to_binary(n, b){
 }
 **/
 
+library Set {
+  // We define a new struct datatype that will be used to
+  // hold its data in the calling contract.
+  struct Data { mapping(uint => bool) flags; }
+
+  // Note that the first parameter is of type "storage
+  // reference" and thus only its storage address and not
+  // its contents is passed as part of the call.  This is a
+  // special feature of library functions.  It is idiomatic
+  // to call the first parameter 'self', if the function can
+  // be seen as a method of that object.
+  function insert(Data storage self, uint value)
+      returns (bool)
+  {
+      if (self.flags[value])
+          return false; // already there
+      self.flags[value] = true;
+      return true;
+  }
+
+  function remove(Data storage self, uint value)
+      returns (bool)
+  {
+      if (!self.flags[value])
+          return false; // not there
+      self.flags[value] = false;
+      return true;
+  }
+
+  function contains(Data storage self, uint value)
+      returns (bool)
+  {
+      return self.flags[value];
+  }
+}
+
+
 contract BytesReader {
-    
-    function byte_to_bits(uint n) returns (uint [] ) {
+    uint[] s;
+    // storage memory
+    function byte_to_bits(uint n) returns (uint[]) {
         uint b = 2;
-        uint [] s;
-        while ( n !== 0 ) {
+        uint idx = 0;
+        while ( n != 0 ) {
             uint m = n % b;
             n = n/b;
             s.push(m);
@@ -28,6 +67,6 @@ contract BytesReader {
         return s;
     }
     function BytesReader (){
-
+        
     }
 }
--- src/sc2/replay.sol
+++ src/sc2/replay.sol
@@ -4,24 +4,23 @@ pragma solidity ^0.4.0;
 
 library Replay {
 	struct Pixel {
-		uint8 r,
-		uint8 g,
-		uint8 b,
-		uint8 a,
+		uint8 r;
+		uint8 g;
+		uint8 b;
+		uint8 a;
 	}
 	struct Player {
-		Pixel color,
-		uint team_id,
-		string race,
-		uint handicap,
-		uint   result,
-		uint workingSetSlotId,
-		string hero,
-		string name,
-
+		Pixel color;
+		uint team_id;
+		string race;
+		uint handicap;
+		uint   result;
+		uint workingSetSlotId;
+		string hero;
+		string name;
 	}
 	struct SCReplay {
-		Player[] players
+		Player[] players;
 	}
 	function read(bytes replay_file) {
 

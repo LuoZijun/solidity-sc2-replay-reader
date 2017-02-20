88e5ed1368b1e58de25340568f3bd4f98e304e5c
Luo <gnulinux@126.com>
2017-02-20 12:17:45 +0800

    update adler32.py

--- adler32.py
+++ adler32.py
@@ -7,27 +7,19 @@ from __future__ import division
 
 # Copy from https://golang.org/src/hash/adler32/adler32.go
 
-# // mod is the largest prime that is less than 65536.
-MOD  = 65521;
+# // BASE is the largest prime that is less than 65536.
+BASE = 65521;
 # // nmax is the largest n such that
-# // 255 * n * (n+1) / 2 + (n+1) * (mod-1) <= 2^32-1.
+# // 255 * n * (n+1) / 2 + (n+1) * (BASE-1) <= 2^32-1.
 # // It is mentioned in RFC 1950 (search for "5552").
 NMAX = 5552;
 
+U32_MAX_VALUE = 4294967295
+U32_MIN_VALUE = 0
+
 def u32(n):
-    # Min: 0, Max: 4294967295
-    min_value = 0
-    max_value = 4294967295
-    if type(n) == float:
-        n = int(n)
-    if n < min_value:
-        return 0
-    elif n >= 0 and n <= max_value:
-        return n
-    elif n > max_value:
-        return u32(n - max_value - 1)
-    else:
-        raise ValueError('Ooops(%d)...' % n)
+    if type(n) == float: n = int(n)
+    return min(max(n, U32_MIN_VALUE), U32_MAX_VALUE)
 
 def update(d, p):
     s1 = u32(d & 0xffff)
@@ -55,8 +47,8 @@ def update(d, p):
             s1 += u32(x)
             s2 += s1
 
-        s1 %= MOD
-        s2 %= MOD
+        s1 %= BASE
+        s2 %= BASE
         p = q
     return u32(s2 << 16 | s1)
 
